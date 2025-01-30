64c6ee3ef95c4da0a8297279e4fa69c0428b43c2
LeoLeerila <leo.leerila@metropolia.fi>
2025-01-30 09:01:15 +0200

    Moduuli5-1.py korjattu ohjeen mukaisiksi

--- Moduuli5/moduuli5-1.py
+++ Moduuli5/moduuli5-1.py
@@ -1,6 +1,9 @@
 import random
 
 kuutioidenmaara = int(input("Syötä arpakuutioiden määrä: "))
+summa = 0
 
 for i in range(0, kuutioidenmaara):
-    print(str(random.randint(1, 6)))
+    summa += random.randint(1, 6)
+
+print(str(summa))
