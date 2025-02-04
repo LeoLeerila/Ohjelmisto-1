6302e79b492a8a60dc83ead85242a22297d87296
LeoLeerila <leo.leerila@metropolia.fi>
2025-02-04 15:04:16 +0200

    moduuli4-6.py muutettu

--- Moduuli4/moduuli4-6.py
+++ Moduuli4/moduuli4-6.py
@@ -1,17 +1,11 @@
 import random
 
-pisteet = []
 pisteidenkokonaismaara = int(input("Syötä arvottavien pisteiden määrä:\n"))
 pisteetympyransisalla = 0
 
-i = 0
-while i < pisteidenkokonaismaara:
-    pisteet.append([random.uniform(-1, 1), random.uniform(-1, 1)])
-    i += 1
-
 for x in range(0, pisteidenkokonaismaara):
-    if ((pisteet[x][0] * pisteet[x][0]) + (pisteet[x][1] * pisteet[x][1])) < 1:
+    if ((random.uniform(-1, 1)**2) + (random.uniform(-1, 1)**2)) < 1:
         pisteetympyransisalla += 1
-    print(f"{x} / {pisteidenkokonaismaara}")
+    #print(f"{x} / {pisteidenkokonaismaara}")
 
 print(f"piin likiarvo on: {(4 * pisteetympyransisalla / pisteidenkokonaismaara)}")
